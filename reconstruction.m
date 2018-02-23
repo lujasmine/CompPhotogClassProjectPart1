716fdd43f37b5411fc73649a92a1a2f618ba57c7
Jasmine Lu <jasminelu.3@gmail.com>
2018-02-23 18:15:33 +0000

    u v codes done

--- reconstruction.m
+++ reconstruction.m
@@ -6,35 +6,33 @@ seq = load_sequence_color('resources/cube_T1', '00', 0, 39, 2, 'png');
 fprintf("Done.\n");
 
 [rows,cols,~,n_imgs] = size(seq);
+bin_vals = [1,2,4,8,16,32,64,128,256,512];
 
 % Convert to grayscale
 for n = 1:size(seq,4)
     seq(:,:,n) = rgb2gray(seq(:,:,:,n));
 end
 
-u_code = zeros(rows,cols);
-v_code = zeros(rows,cols);
-
 img_index = 1;
-diff = zeros(rows, cols, 10);
+diff = zeros(rows, cols, 20);
 
 for k = 1 : n_imgs / 2
     diff(:,:,k) = seq(:,:,img_index) - seq(:,:,img_index + 1);
     img_index = img_index + 2;
 end
 
-% TODO filter unknown picels
+% TODO filter unknown pixels
 
 diff(diff >= 0) = 1;
 diff(diff < 0) = 0;
 
+u_code = zeros(rows,cols);
+v_code = zeros(rows,cols);
+
 for i = 1 : rows
     disp(i);
     for j = 1 : cols
-        u_str = num2str(diff(i,j,1:10));
-        u_code(i,j) = bin2dec(u_str) + 1;
-        
-        v_str = num2str(diff(i,j,11:20));
-        v_code(i,j) = bin2dec(v_str) + 1;
+        u_code(i,j) = sum(reshape(diff(i,j,1:10),1,10).*bin_vals);
+        v_code(i,j) = sum(reshape(diff(i,j,11:20),1,10).*bin_vals);
     end
 end
